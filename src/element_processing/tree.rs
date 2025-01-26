benjamin051000/arnis

element_processing/tree: DRY on fill calls, use j with y.
This commit is a little messy:
- It reduces repitition of the block fill calls and the snow layer calls
- It also replaces previous usage of `y + i` with `y + j` to keep the
  variable conventions the same.

src/element_processing/tree.rs
@@ -75,20 +75,20 @@ pub fn create_tree(editor: &mut WorldEditor, x: i32, y: i32, z: i32, typetree: u
                 ((0, 3, 1), (0, 9, 1)),
                 ((0, 9, 0), (0, 10, 0)),
             ];
-            for ((x1, y1, z1), (x2, y2, z2)) in leaves_fill_coords {
-                editor.fill_blocks(OAK_LEAVES, x1, y1, z1, x2, y2, z2, None, None);
+            for ((i1, j1, k1), (i2, j2, k2)) in leaves_fill_coords {
+                editor.fill_blocks(OAK_LEAVES, x + i1, y + j1, z + k1, x + i2, y + j2, z + k2, None, None);
             }
 
-            for i in (3..=8).rev() {
-                round(editor, OAK_LEAVES, x, y + i, z, &ROUND1_COORDS);
+            for j in (3..=8).rev() {
+                round(editor, OAK_LEAVES, x, y + j, z, &ROUND1_COORDS);
             }
 
-            for i in (4..=7).rev() {
-                round(editor, OAK_LEAVES, x, y + i, z, &ROUND2_COORDS);
+            for j in (4..=7).rev() {
+                round(editor, OAK_LEAVES, x, y + j, z, &ROUND2_COORDS);
             }
 
-            for i in (5..=6).rev() {
-                round(editor, OAK_LEAVES, x, y + i, z, &ROUND3_COORDS);
+            for j in (5..=6).rev() {
+                round(editor, OAK_LEAVES, x, y + j, z, &ROUND3_COORDS);
             }
 
             if snow {
@@ -99,86 +99,107 @@ pub fn create_tree(editor: &mut WorldEditor, x: i32, y: i32, z: i32, typetree: u
                     (0, 10, -1),
                     (0, 10, 1),
                 ];
-                for (x, y, z) in snow_coords {
-                    editor.set_block(SNOW_LAYER, x, y, z, None, None);
+                for (i, j, k) in snow_coords {
+                    editor.set_block(SNOW_LAYER, x + i, y + j, z + k, None, None);
                 }
 
-                for i in (6..=9).rev() {
-                    round(editor, SNOW_LAYER, x, y + i, z, &ROUND1_COORDS);
+                for j in (6..=9).rev() {
+                    round(editor, SNOW_LAYER, x, y + j, z, &ROUND1_COORDS);
                 }
 
-                for i in (5..=8).rev() {
-                    round(editor, SNOW_LAYER, x, y + i, z, &ROUND2_COORDS);
+                for j in (5..=8).rev() {
+                    round(editor, SNOW_LAYER, x, y + j, z, &ROUND2_COORDS);
                 }
 
-                for i in (6..=7).rev() {
-                    round(editor, SNOW_LAYER, x, y + i, z, &ROUND3_COORDS);
+                for j in (6..=7).rev() {
+                    round(editor, SNOW_LAYER, x, y + j, z, &ROUND3_COORDS);
                 }
             }
         }
         2 => {
             // Spruce tree
             editor.fill_blocks(SPRUCE_LOG, x, y, z, x, y + 9, z, None, None);
-            editor.fill_blocks(BIRCH_LEAVES, x - 1, y + 3, z, x - 1, y + 10, z, None, None);
-            editor.fill_blocks(BIRCH_LEAVES, x + 1, y + 3, z, x + 1, y + 10, z, None, None);
-            editor.fill_blocks(BIRCH_LEAVES, x, y + 3, z - 1, x, y + 10, z - 1, None, None);
-            editor.fill_blocks(BIRCH_LEAVES, x, y + 3, z + 1, x, y + 10, z + 1, None, None);
+            let birch_leaves_fill = [
+                ((-1, 3, 0), (-1, 10, 0)),
+                ((0, 3, -1), (0, 10, -1)),
+                ((1, 3, 0), (1, 10, 0)),
+                ((0, 3, -1), (0, 10, -1)),
+                ((0, 3, 1), (0, 10, 1)),
+            ];
+            for ((i1, j1, k1), (i2, j2, k2)) in birch_leaves_fill {
+                editor.fill_blocks(BIRCH_LEAVES, x + i1, y + j1, z + k1, x + i2, y + j2, z + k2, None, None);
+            }
             editor.set_block(BIRCH_LEAVES, x, y + 10, z, None, None);
 
-            for i in [9, 7, 6, 4, 3] {
-                round(editor, BIRCH_LEAVES, x, y + i, z, &ROUND1_COORDS);
+            for j in [9, 7, 6, 4, 3] {
+                round(editor, BIRCH_LEAVES, x, y + j, z, &ROUND1_COORDS);
             }
 
-            for i in [6, 3] {
-                round(editor, BIRCH_LEAVES, x, y + i, z, &ROUND2_COORDS);
+            for j in [6, 3] {
+                round(editor, BIRCH_LEAVES, x, y + j, z, &ROUND2_COORDS);
             }
 
             if snow {
-                editor.set_block(SNOW_LAYER, x, y + 11, z, None, None);
-                editor.set_block(SNOW_LAYER, x + 1, y + 11, z, None, None);
-                editor.set_block(SNOW_LAYER, x - 1, y + 11, z, None, None);
-                editor.set_block(SNOW_LAYER, x, y + 11, z - 1, None, None);
-                editor.set_block(SNOW_LAYER, x, y + 11, z + 1, None, None);
-
-                for i in [10, 8, 7, 5, 4] {
-                    round(editor, SNOW_LAYER, x, y + i, z, &ROUND1_COORDS);
+                let snow_fill = [
+                    (0, 11, 0),
+                    (1, 11, 0),
+                    (-1, 11, 0),
+                    (0, 11, -1),
+                    (0, 11, 1),
+                ];
+                for (i, j, k) in snow_fill {
+                    editor.set_block(SNOW_LAYER, x + i, y + j, z + k, None, None);
                 }
 
-                for i in [7, 4] {
-                    round(editor, SNOW_LAYER, x, y + i, z, &ROUND2_COORDS);
+                for j in [10, 8, 7, 5, 4] {
+                    round(editor, SNOW_LAYER, x, y + j, z, &ROUND1_COORDS);
+                }
+
+                for j in [7, 4] {
+                    round(editor, SNOW_LAYER, x, y + j, z, &ROUND2_COORDS);
                 }
             }
         }
         3 => {
             // Birch tree
             editor.fill_blocks(BIRCH_LOG, x, y, z, x, y + 6, z, None, None);
-            editor.fill_blocks(BIRCH_LEAVES, x - 1, y + 2, z, x - 1, y + 7, z, None, None);
-            editor.fill_blocks(BIRCH_LEAVES, x + 1, y + 2, z, x + 1, y + 7, z, None, None);
-            editor.fill_blocks(BIRCH_LEAVES, x, y + 2, z - 1, x, y + 7, z - 1, None, None);
-            editor.fill_blocks(BIRCH_LEAVES, x, y + 2, z + 1, x, y + 7, z + 1, None, None);
-            editor.fill_blocks(BIRCH_LEAVES, x, y + 7, z, x, y + 8, z, None, None);
-
-            for i in (2..=6).rev() {
-                round(editor, BIRCH_LEAVES, x, y + i, z, &ROUND1_COORDS);
+            let leaves_fills = [
+                ((-1, 2, 0), (-1, 7, 0)),
+                ((1, 2, 0), (1, 7, 0)),
+                ((0, 2, -1), (0, 7, -1)),
+                ((0, 2, 1), (0, 7, 1)),
+                ((0, 7, 0), (0, 8, 0)),
+            ];
+            for ((i1, j1, k1), (i2, j2, k2)) in leaves_fills {
+                editor.fill_blocks(BIRCH_LEAVES, x + i1, y + j1, z + k1, x + i2, y + j2, z + k2, None, None);
             }
 
-            for i in 2..=4 {
-                round(editor, BIRCH_LEAVES, x, y + i, z, &ROUND2_COORDS);
+            for j in (2..=6).rev() {
+                round(editor, BIRCH_LEAVES, x, y + j, z, &ROUND1_COORDS);
+            }
+
+            for j in 2..=4 {
+                round(editor, BIRCH_LEAVES, x, y + j, z, &ROUND2_COORDS);
             }
 
             if snow {
-                editor.set_block(SNOW_LAYER, x, y + 9, z, None, None);
-                editor.set_block(SNOW_LAYER, x + 1, y + 8, z, None, None);
-                editor.set_block(SNOW_LAYER, x - 1, y + 8, z, None, None);
-                editor.set_block(SNOW_LAYER, x, y + 8, z - 1, None, None);
-                editor.set_block(SNOW_LAYER, x, y + 8, z + 1, None, None);
-
-                for i in (3..=7).rev() {
-                    round(editor, SNOW_LAYER, x, y + i, z, &ROUND1_COORDS);
+                let snow_coords = [
+                    (0, 9, 0),
+                    (1, 8, 0),
+                    (-1, 8, 0),
+                    (0, 8, -1),
+                    (0, 8, 1),
+                ];
+                for (i, j, k) in snow_coords {
+                    editor.set_block(SNOW_LAYER, x + i, y + j, z + k, None, None);
                 }
 
-                for i in 3..=5 {
-                    round(editor, SNOW_LAYER, x, y + i, z, &ROUND2_COORDS);
+                for j in (3..=7).rev() {
+                    round(editor, SNOW_LAYER, x, y + j, z, &ROUND1_COORDS);
+                }
+
+                for j in 3..=5 {
+                    round(editor, SNOW_LAYER, x, y + j, z, &ROUND2_COORDS);
                 }
             }
         }

my1172-tech/ArnisPLATEAU

src/element_processing/tree.rs
@@ -2,7 +2,7 @@ use crate::block_definitions::*;
 use crate::world_editor::WorldEditor;
 
 /// A circular pattern around a central point.
-const ROUND1_COORDS: [(i32, i32, i32); 8] = [
+const ROUND1_PATTERN: [(i32, i32, i32); 8] = [
     (-2, 0, 0),
     (2, 0, 0),
     (0, 0, -2),
@@ -14,7 +14,7 @@ const ROUND1_COORDS: [(i32, i32, i32); 8] = [
 ];
 
 /// A wider circular pattern.
-const ROUND2_COORDS: [(i32, i32, i32); 12] = [
+const ROUND2_PATTERN: [(i32, i32, i32); 12] = [
     (3, 0, 0),
     (2, 0, -1),
     (2, 0, 1),
@@ -30,7 +30,7 @@ const ROUND2_COORDS: [(i32, i32, i32); 12] = [
 ];
 
 /// A more scattered circular pattern.
-const ROUND3_COORDS: [(i32, i32, i32); 12] = [
+const ROUND3_PATTERN: [(i32, i32, i32); 12] = [
     (3, 0, -1),
     (3, 0, 1),
     (2, 0, -2),
@@ -75,20 +75,20 @@ pub fn create_tree(editor: &mut WorldEditor, x: i32, y: i32, z: i32, typetree: u
                 ((0, 3, 1), (0, 9, 1)),
                 ((0, 9, 0), (0, 10, 0)),
             ];
-            for ((x1, y1, z1), (x2, y2, z2)) in leaves_fill_coords {
-                editor.fill_blocks(OAK_LEAVES, x1, y1, z1, x2, y2, z2, None, None);
+            for ((i1, j1, k1), (i2, j2, k2)) in leaves_fill_coords {
+                editor.fill_blocks(OAK_LEAVES, x + i1, y + j1, z + k1, x + i2, y + j2, z + k2, None, None);
             }
 
-            for i in (3..=8).rev() {
-                round(editor, OAK_LEAVES, x, y + i, z, &ROUND1_COORDS);
+            for j in (3..=8).rev() {
+                round(editor, OAK_LEAVES, x, y + j, z, &ROUND1_PATTERN);
             }
 
-            for i in (4..=7).rev() {
-                round(editor, OAK_LEAVES, x, y + i, z, &ROUND2_COORDS);
+            for j in (4..=7).rev() {
+                round(editor, OAK_LEAVES, x, y + j, z, &ROUND2_PATTERN);
             }
 
-            for i in (5..=6).rev() {
-                round(editor, OAK_LEAVES, x, y + i, z, &ROUND3_COORDS);
+            for j in (5..=6).rev() {
+                round(editor, OAK_LEAVES, x, y + j, z, &ROUND3_PATTERN);
             }
 
             if snow {
@@ -99,86 +99,107 @@ pub fn create_tree(editor: &mut WorldEditor, x: i32, y: i32, z: i32, typetree: u
                     (0, 10, -1),
                     (0, 10, 1),
                 ];
-                for (x, y, z) in snow_coords {
-                    editor.set_block(SNOW_LAYER, x, y, z, None, None);
+                for (i, j, k) in snow_coords {
+                    editor.set_block(SNOW_LAYER, x + i, y + j, z + k, None, None);
                 }
 
-                for i in (6..=9).rev() {
-                    round(editor, SNOW_LAYER, x, y + i, z, &ROUND1_COORDS);
+                for j in (6..=9).rev() {
+                    round(editor, SNOW_LAYER, x, y + j, z, &ROUND1_PATTERN);
                 }
 
-                for i in (5..=8).rev() {
-                    round(editor, SNOW_LAYER, x, y + i, z, &ROUND2_COORDS);
+                for j in (5..=8).rev() {
+                    round(editor, SNOW_LAYER, x, y + j, z, &ROUND2_PATTERN);
                 }
 
-                for i in (6..=7).rev() {
-                    round(editor, SNOW_LAYER, x, y + i, z, &ROUND3_COORDS);
+                for j in (6..=7).rev() {
+                    round(editor, SNOW_LAYER, x, y + j, z, &ROUND3_PATTERN);
                 }
             }
         }
         2 => {
             // Spruce tree
             editor.fill_blocks(SPRUCE_LOG, x, y, z, x, y + 9, z, None, None);
-            editor.fill_blocks(BIRCH_LEAVES, x - 1, y + 3, z, x - 1, y + 10, z, None, None);
-            editor.fill_blocks(BIRCH_LEAVES, x + 1, y + 3, z, x + 1, y + 10, z, None, None);
-            editor.fill_blocks(BIRCH_LEAVES, x, y + 3, z - 1, x, y + 10, z - 1, None, None);
-            editor.fill_blocks(BIRCH_LEAVES, x, y + 3, z + 1, x, y + 10, z + 1, None, None);
+            let birch_leaves_fill = [
+                ((-1, 3, 0), (-1, 10, 0)),
+                ((0, 3, -1), (0, 10, -1)),
+                ((1, 3, 0), (1, 10, 0)),
+                ((0, 3, -1), (0, 10, -1)),
+                ((0, 3, 1), (0, 10, 1)),
+            ];
+            for ((i1, j1, k1), (i2, j2, k2)) in birch_leaves_fill {
+                editor.fill_blocks(BIRCH_LEAVES, x + i1, y + j1, z + k1, x + i2, y + j2, z + k2, None, None);
+            }
             editor.set_block(BIRCH_LEAVES, x, y + 10, z, None, None);
 
-            for i in [9, 7, 6, 4, 3] {
-                round(editor, BIRCH_LEAVES, x, y + i, z, &ROUND1_COORDS);
+            for j in [9, 7, 6, 4, 3] {
+                round(editor, BIRCH_LEAVES, x, y + j, z, &ROUND1_PATTERN);
             }
 
-            for i in [6, 3] {
-                round(editor, BIRCH_LEAVES, x, y + i, z, &ROUND2_COORDS);
+            for j in [6, 3] {
+                round(editor, BIRCH_LEAVES, x, y + j, z, &ROUND2_PATTERN);
             }
 
             if snow {
-                editor.set_block(SNOW_LAYER, x, y + 11, z, None, None);
-                editor.set_block(SNOW_LAYER, x + 1, y + 11, z, None, None);
-                editor.set_block(SNOW_LAYER, x - 1, y + 11, z, None, None);
-                editor.set_block(SNOW_LAYER, x, y + 11, z - 1, None, None);
-                editor.set_block(SNOW_LAYER, x, y + 11, z + 1, None, None);
-
-                for i in [10, 8, 7, 5, 4] {
-                    round(editor, SNOW_LAYER, x, y + i, z, &ROUND1_COORDS);
+                let snow_fill = [
+                    (0, 11, 0),
+                    (1, 11, 0),
+                    (-1, 11, 0),
+                    (0, 11, -1),
+                    (0, 11, 1),
+                ];
+                for (i, j, k) in snow_fill {
+                    editor.set_block(SNOW_LAYER, x + i, y + j, z + k, None, None);
                 }
 
-                for i in [7, 4] {
-                    round(editor, SNOW_LAYER, x, y + i, z, &ROUND2_COORDS);
+                for j in [10, 8, 7, 5, 4] {
+                    round(editor, SNOW_LAYER, x, y + j, z, &ROUND1_PATTERN);
+                }
+
+                for j in [7, 4] {
+                    round(editor, SNOW_LAYER, x, y + j, z, &ROUND2_PATTERN);
                 }
             }
         }
         3 => {
             // Birch tree
             editor.fill_blocks(BIRCH_LOG, x, y, z, x, y + 6, z, None, None);
-            editor.fill_blocks(BIRCH_LEAVES, x - 1, y + 2, z, x - 1, y + 7, z, None, None);
-            editor.fill_blocks(BIRCH_LEAVES, x + 1, y + 2, z, x + 1, y + 7, z, None, None);
-            editor.fill_blocks(BIRCH_LEAVES, x, y + 2, z - 1, x, y + 7, z - 1, None, None);
-            editor.fill_blocks(BIRCH_LEAVES, x, y + 2, z + 1, x, y + 7, z + 1, None, None);
-            editor.fill_blocks(BIRCH_LEAVES, x, y + 7, z, x, y + 8, z, None, None);
-
-            for i in (2..=6).rev() {
-                round(editor, BIRCH_LEAVES, x, y + i, z, &ROUND1_COORDS);
+            let leaves_fills = [
+                ((-1, 2, 0), (-1, 7, 0)),
+                ((1, 2, 0), (1, 7, 0)),
+                ((0, 2, -1), (0, 7, -1)),
+                ((0, 2, 1), (0, 7, 1)),
+                ((0, 7, 0), (0, 8, 0)),
+            ];
+            for ((i1, j1, k1), (i2, j2, k2)) in leaves_fills {
+                editor.fill_blocks(BIRCH_LEAVES, x + i1, y + j1, z + k1, x + i2, y + j2, z + k2, None, None);
             }
 
-            for i in 2..=4 {
-                round(editor, BIRCH_LEAVES, x, y + i, z, &ROUND2_COORDS);
+            for j in (2..=6).rev() {
+                round(editor, BIRCH_LEAVES, x, y + j, z, &ROUND1_PATTERN);
+            }
+
+            for j in 2..=4 {
+                round(editor, BIRCH_LEAVES, x, y + j, z, &ROUND2_PATTERN);
             }
 
             if snow {
-                editor.set_block(SNOW_LAYER, x, y + 9, z, None, None);
-                editor.set_block(SNOW_LAYER, x + 1, y + 8, z, None, None);
-                editor.set_block(SNOW_LAYER, x - 1, y + 8, z, None, None);
-                editor.set_block(SNOW_LAYER, x, y + 8, z - 1, None, None);
-                editor.set_block(SNOW_LAYER, x, y + 8, z + 1, None, None);
-
-                for i in (3..=7).rev() {
-                    round(editor, SNOW_LAYER, x, y + i, z, &ROUND1_COORDS);
+                let snow_coords = [
+                    (0, 9, 0),
+                    (1, 8, 0),
+                    (-1, 8, 0),
+                    (0, 8, -1),
+                    (0, 8, 1),
+                ];
+                for (i, j, k) in snow_coords {
+                    editor.set_block(SNOW_LAYER, x + i, y + j, z + k, None, None);
                 }
 
-                for i in 3..=5 {
-                    round(editor, SNOW_LAYER, x, y + i, z, &ROUND2_COORDS);
+                for j in (3..=7).rev() {
+                    round(editor, SNOW_LAYER, x, y + j, z, &ROUND1_PATTERN);
+                }
+
+                for j in 3..=5 {
+                    round(editor, SNOW_LAYER, x, y + j, z, &ROUND2_PATTERN);
                 }
             }
         }